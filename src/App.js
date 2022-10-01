b3d51e9e3344a1cfb5d2c1478aca3809a2cc87c3
faazizpro <fahadafs11@gmail.com>
2022-10-02 01:38:03 +0600

    Portfolio Section Starts

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Service from './components/Service/Service';
 import About from './components/About/About';
+import Portfolio from './components/Portfolio/Portfolio';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
          <Hero></Hero>
          <Service></Service>
          <About></About>
+         <Portfolio></Portfolio>
     </div>
   );
 }
---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div className='pt-20 pb-20' id='service-bg'>
+            <div className='text-center'>
+                <h3 className='text-2xl font-bold mb-5'>Portfolio</h3>
+                <h2 className='text-5xl font-bold'>Our Recent Works</h2>
+                <p className='mt-5 font-medium text-center'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nobis animi <br /> tempora quo vero cumque iusto  autem magni, error nulla praesentium? <br /> Eveniet possimus voluptas necessitatibus!
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Portfolio;
