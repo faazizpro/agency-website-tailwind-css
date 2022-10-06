7a1883e20111172d07b04b1c9ac3a16cf6690199
faazizpro <fahadafs11@gmail.com>
2022-10-06 11:07:39 +0600

    Team Member Images Added

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,11 +1,107 @@
 import React from 'react';
 import './Team.css'
+import member1 from '../../images/member 1.jpg'
+import member2 from '../../images/member 2.jpg'
+
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faPinterestP } from '@fortawesome/free-brands-svg-icons'
+import { faDribbble } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 
 const Team = () => {
     return (
-        <div>
-            
+        <div className='team-bg pt-20'>
+            <div className='text-center mb-10'>
+                <h3 className='text-2xl font-bold mb-5'>Meet Our Team</h3>
+                <h2 className='text-5xl font-bold'>Our Expert Members</h2>
+                <p className='mt-5 font-medium text-center'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nobis animi <br /> tempora quo vero cumque iusto  autem magni.
+                </p>
+            </div>
+
+            {/* Team Member Starts */}
+            <div id='main-team-section' className='pl-20 pb-20 flex justify-center gap-24   '>
+                <div id='team-card' className='w-64 relative'>
+                    <img src={member1} className='w-64' alt="" />
+
+                    {/* Team Icon & Info */}
+                    <div className='bg-white rounded w-5/6 absolute top-64 left-5 drop-shadow-xl'>
+                        {/* Icons */}
+                        <div className='flex gap-5  pt-3 pb-2 pl-3 '>
+                            <FontAwesomeIcon icon={faFacebookF} />
+                            <FontAwesomeIcon icon={faPinterestP} />
+                            <FontAwesomeIcon icon={faDribbble} />
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </div>
+                        {/* Team Info */}
+                        <div className='pl-3 pb-2'>
+                            <h3 className='text-2xl font-bold'>Jane Cooper</h3>
+                            <p className='text-gray-500 font-semibold text-lg'>Developer</p>
+                        </div>
+                    </div>
+                </div>
+                <div id='team-card' className='w-64 relative'>
+                    <img src={member2} className='w-64' alt="" />
+
+                    {/* Team Icon & Info */}
+                    <div className='bg-white rounded w-5/6 absolute top-64 left-5 drop-shadow-xl'>
+                        {/* Icons */}
+                        <div className='flex gap-5  pt-3 pb-2 pl-3 '>
+                            <FontAwesomeIcon icon={faFacebookF} />
+                            <FontAwesomeIcon icon={faPinterestP} />
+                            <FontAwesomeIcon icon={faDribbble} />
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </div>
+                        {/* Team Info */}
+                        <div className='pl-3 pb-2'>
+                            <h3 className='text-2xl font-bold'>Jane Cooper</h3>
+                            <p className='text-gray-500 font-semibold text-lg'>Developer</p>
+                        </div>
+                    </div>
+                </div>
+                <div id='team-card' className='w-64 relative'>
+                    <img src={member2} className='w-64' alt="" />
+
+                    {/* Team Icon & Info */}
+                    <div className='bg-white rounded w-5/6 absolute top-64 left-5 drop-shadow-xl'>
+                        {/* Icons */}
+                        <div className='flex gap-5  pt-3 pb-2 pl-3 '>
+                            <FontAwesomeIcon icon={faFacebookF} />
+                            <FontAwesomeIcon icon={faPinterestP} />
+                            <FontAwesomeIcon icon={faDribbble} />
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </div>
+                        {/* Team Info */}
+                        <div className='pl-3 pb-2'>
+                            <h3 className='text-2xl font-bold'>Jane Cooper</h3>
+                            <p className='text-gray-500 font-semibold text-lg'>Developer</p>
+                        </div>
+                    </div>
+                </div>
+                <div id='team-card' className='w-64 relative'>
+                    <img src={member2} className='w-64' alt="" />
+
+                    {/* Team Icon & Info */}
+                    <div className='bg-white rounded w-5/6 absolute top-64 left-5 drop-shadow-xl'>
+                        {/* Icons */}
+                        <div className='flex gap-5  pt-3 pb-2 pl-3 '>
+                            <FontAwesomeIcon icon={faFacebookF} />
+                            <FontAwesomeIcon icon={faPinterestP} />
+                            <FontAwesomeIcon icon={faDribbble} />
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </div>
+                        {/* Team Info */}
+                        <div className='pl-3 pb-2'>
+                            <h3 className='text-2xl font-bold'>Jane Cooper</h3>
+                            <p className='text-gray-500 font-semibold text-lg'>Developer</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
